79223a79101aa8b1b7f0adc76974fd133443a554
hassan alhassaini <hassandak1d@hotmail.com>
2021-09-20 22:58:58 +0300

    1st draft

--- calculator.js
+++ calculator.js
@@ -2,5 +2,8 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+// const name = prompt("What's your name? ");
+// console.log(`Hi, I'm ${name}.`);
+
+let integer1 = prompt("select your first number");
+let integer2 = prompt("select your second number? ");
